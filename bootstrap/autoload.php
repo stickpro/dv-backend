87f3690731de4bc792ed3abfe197cfe6f22804ec
stickpro <stick.qwe@gmail.com>
2023-10-29 20:50:41 +0300

    upd autoload

--- bootstrap/autoload.php
+++ bootstrap/autoload.php
@@ -2,25 +2,6 @@
 
 define('LARAVEL_START', microtime(true));
 
-/*
-|--------------------------------------------------------------------------
-| Register Core Helpers
-|--------------------------------------------------------------------------
-|
-| We cannot rely on Composer's load order when calculating the weight of
-| each package. This line ensures that the core global helpers are
-| always given priority one status.
-|
-*/
-
-$helperPath = __DIR__.'/../vendor/winter/storm/src/Support/helpers.php';
-
-if (!file_exists($helperPath)) {
-    echo 'Missing vendor files, try running "composer install" or use the Wizard installer.'.PHP_EOL;
-    exit(1);
-}
-
-require $helperPath;
 
 /*
 |--------------------------------------------------------------------------
